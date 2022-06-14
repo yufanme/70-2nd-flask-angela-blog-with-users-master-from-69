5d4a3684e5d1d05e3014dbba2fb0e181b667c5b3
yufanme <562937707@qq.com>
2022-06-14 17:48:24 +0800

    change English to han some pages.

--- forms.py
+++ forms.py
@@ -6,26 +6,26 @@ from flask_ckeditor import CKEditorField
 
 # WTForm
 class CreatePostForm(FlaskForm):
-    title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = CKEditorField("Blog Content", validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
+    title = StringField("博文标题", validators=[DataRequired()])
+    subtitle = StringField("副标题", validators=[DataRequired()])
+    img_url = StringField("博文图片链接", validators=[DataRequired(), URL()])
+    body = CKEditorField("博文内容", validators=[DataRequired()])
+    submit = SubmitField("提交博文")
 
 
 class RegisterForm(FlaskForm):
-    email = StringField(label="Email", validators=[DataRequired()])
-    password = PasswordField(label="Password", validators=[DataRequired()])
-    name = StringField(label="Name", validators=[DataRequired()])
-    submit = SubmitField(label="SIGN ME UP!")
+    email = StringField(label="邮箱", validators=[DataRequired()])
+    password = PasswordField(label="密码", validators=[DataRequired()])
+    name = StringField(label="昵称", validators=[DataRequired()])
+    submit = SubmitField(label="注册！")
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label="Email", validators=[DataRequired()])
-    password = PasswordField(label="Password", validators=[DataRequired()])
-    submit = SubmitField(label="LET ME IN!")
+    email = StringField(label="邮箱", validators=[DataRequired()])
+    password = PasswordField(label="密码", validators=[DataRequired()])
+    submit = SubmitField(label="登陆！")
 
 
 class CommentForm(FlaskForm):
-    comment = CKEditorField(label="Comment", validators=[DataRequired()])
-    submit = SubmitField(label="SUBMIT COMMENT")
+    comment = CKEditorField(label="评论", validators=[DataRequired()])
+    submit = SubmitField(label="提交评论")
